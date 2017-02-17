78e9f6bf653c96afc3b7fc8aec65c56ce211fd54
Devagupthapu <krishna.devagupthapu@accenture.com>
2017-02-17 12:04:50 +0800

    Commiting my changes to local repositary

--- src/main/java/AnnotationTest/AnnotationTest/App.java
+++ src/main/java/AnnotationTest/AnnotationTest/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! I modified from git repositary" );
+		System.out.println( "I changed from my local repositary" );
     }
 }
