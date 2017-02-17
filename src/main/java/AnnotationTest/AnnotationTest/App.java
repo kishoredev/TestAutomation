230252a2c28523043499717a5ce6422549c87301
kishoredev <kishoreniyogi@gmail.com>
2017-02-17 12:00:41 +0800

    2

--- src/main/java/AnnotationTest/AnnotationTest/App.java
+++ src/main/java/AnnotationTest/AnnotationTest/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! I modified from git repositary" );
+        System.out.println( "added line from git repositary" );
     }
 }
