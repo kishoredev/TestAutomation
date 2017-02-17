d3dcf897ca7d862d0dd7e4170663d185288da605
kishoredev <kishoreniyogi@gmail.com>
2017-02-17 11:58:30 +0800

    Commit Sample

--- src/main/java/AnnotationTest/AnnotationTest/App.java
+++ src/main/java/AnnotationTest/AnnotationTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! I modified" );
+        System.out.println( "Hello World! I modified from git repositary" );
     }
 }
